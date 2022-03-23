dac5d9cb6777930a9b464536874c7fa2e50d3041
Arslan Zia <ziaarslan13@gmail.com>
2022-03-24 00:59:08 +0500

    final commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "bookmark-purple":"#5267DF",
+        "bookmark-red":"#FA5959",
+        "bookmark-blue":"#242A45",
+        "bookmark-grey":"#9194A2",
+        "bookmark-white":"#f7f7f7",
+      },
+    },
+    
+    fontFamily:{
+      Lato:["Lato, sans-serif"],
+    },
+    container:{
+      center: true,
+    }
+    
+  },
+  plugins: [],
+}
